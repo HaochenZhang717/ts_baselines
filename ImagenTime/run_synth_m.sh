c14e3bff2ed0bfec24e9cfb603d88dec450bc718
Haochen Zhang <hz112@rice.edu>
2026-03-18 19:47:46 -0500

    0318

---
+++ ImagenTime/run_synth_m.sh
@@ -0,0 +1,3 @@
+export CUDA_VISIBLE_DEVICES=1
+python run_unconditional.py \
+  --config ./configs/neurips_baseline/synth_m.yaml
--- ImagenTime/run_synth_u.sh
+++ ImagenTime/run_synth_u.sh
@@ -1,3 +1,3 @@
 export CUDA_VISIBLE_DEVICES=0
 python run_unconditional.py \
-  --config ./configs/unconditional/synth_u.yaml
+  --config ./configs/neurips_baseline/synth_u.yaml
