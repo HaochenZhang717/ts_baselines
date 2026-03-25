aedeb2246c519db3961a1fbf5bcdfcd8e3168b35
Haochen Zhang <hz112@rice.edu>
2026-03-25 16:53:00 -0500

    0325

--- ImagenTime/run_synth_u.sh
+++ ImagenTime/run_synth_u.sh
@@ -2,5 +2,7 @@ export CUDA_VISIBLE_DEVICES=4
 #python run_unconditional.py \
 #  --config ./configs/neurips_baseline/synth_u.yaml
 
+export WANDB_PROJECT="unconditional_baselines"
+export WANDB_NAME="VerbalTS_uncond_EDM"
 python run_unconditional_my_model.py \
   --config ./configs/neurips_baseline/synth_u.yaml
