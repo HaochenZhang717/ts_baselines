c33640f8d2bc927cd67f31c5c0ee0eb224a0883e
Haochen Zhang <hz112@rice.edu>
2026-03-25 17:13:08 -0500

    0325

--- ImagenTime/run_unconditional_my_model.py
+++ ImagenTime/run_unconditional_my_model.py
@@ -121,10 +121,10 @@ def main(args):
                 gen_sig = np.vstack(gen_sig)
                 real_sig = np.vstack(real_sig)
                 # print(f"gen_sig: {gen_sig.shape}, real_sig: {real_sig.shape}")
-                scores = evaluate_model_uncond(real_sig, gen_sig, args)
-                for key, value in scores.items():
-                    logger.log(f'test/{key}', value, epoch)
-                breakpoint()
+                # scores = evaluate_model_uncond(real_sig, gen_sig, args)
+                # for key, value in scores.items():
+                #     logger.log(f'test/{key}', value, epoch)
+                # breakpoint()
                 save_path = os.path.join(args.log_dir, f"samples_epoch_{epoch}.pt")
                 torch.save({
                     "gen_ts": torch.from_numpy(gen_sig),  # (N, T, D)
