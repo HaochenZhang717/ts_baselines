cefe6537433b1799c9a1cae3eb19f4fbaea7203f
Haochen Zhang <hz112@rice.edu>
2026-03-26 13:40:14 -0500

    0326

--- ImagenTime/run_synth_u_imagen_time_text_ldm.sh
+++ ImagenTime/run_synth_u_imagen_time_text_ldm.sh
@@ -5,5 +5,5 @@ export WANDB_NAME="LDM_debug"
 
 python run_text_conditional_ldm.py \
   --config ./configs/neurips_baseline/synth_u_text_conditional_ldm.yaml \
-  --log_dir ../edm_results_text_conditional/imagen_time \
+  --log_dir ../edm_results_text_conditional/LDM_debug \
   --model_type LDMUNetModel
