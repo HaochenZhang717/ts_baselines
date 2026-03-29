978ca83c61c9f9475acf527e52c9318db2d624e6
Haochen Zhang <hz112@rice.edu>
2026-03-29 17:00:48 -0500

    0329

--- ImagenTime/run_aireadi_glucose.sh
+++ ImagenTime/run_aireadi_glucose.sh
@@ -4,7 +4,8 @@ export WANDB_PROJECT="aireadi_prediction"
 export WANDB_NAME="glucose"
 
 python run_prediction.py \
-  --config ./configs/extrapolation/aireadi_glucose.yaml
+  --config ./configs/extrapolation/aireadi_glucose.yaml \
+  --logdir ./results_aireadi/glucose
 
 
 
--- ImagenTime/run_aireadi_glucose_calorie.sh
+++ ImagenTime/run_aireadi_glucose_calorie.sh
@@ -4,7 +4,8 @@ export WANDB_PROJECT="aireadi_prediction"
 export WANDB_NAME="glucose_calorie"
 
 python run_prediction.py \
-  --config ./configs/extrapolation/aireadi_glucose_calorie.yaml
+  --config ./configs/extrapolation/aireadi_glucose_calorie.yaml \
+  --logdir ./results_aireadi/glucose_calorie
 
 
 
