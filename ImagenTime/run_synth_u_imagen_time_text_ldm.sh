555f86902eb732b9c17555044f28bdce9616ed0b
Haochen Zhang <hz112@rice.edu>
2026-03-28 23:23:55 -0500

    0328

--- ImagenTime/run_synth_u_imagen_time_text_ldm.sh
+++ ImagenTime/run_synth_u_imagen_time_text_ldm.sh
@@ -6,4 +6,5 @@ export WANDB_NAME="LDM_debug"
 python run_text_conditional_ldm.py \
   --config ./configs/neurips_baseline/synth_u_text_conditional_ldm.yaml \
   --log_dir ../edm_results_text_conditional/LDM_debug \
-  --model_type LDMUNetModel
+  --model_type LDMUNetModel \
+  --fid_vae_ckpt_path "/playpen-shared/haochenz/fid_vae_ckpts/vae_synth_u/best.pt"
