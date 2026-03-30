daa6222e9a9e0fc33125fb556c44ac899223b363
Haochen Zhang <hz112@rice.edu>
2026-03-30 15:49:22 -0500

    0330

--- ImagenTime/run_aireadi_glucose.sh
+++ ImagenTime/run_aireadi_glucose.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=5
+export CUDA_VISIBLE_DEVICES=2
 
 export WANDB_PROJECT="aireadi_prediction"
 export WANDB_NAME="glucose"
--- ImagenTime/run_aireadi_glucose_calorie.sh
+++ ImagenTime/run_aireadi_glucose_calorie.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=6
+export CUDA_VISIBLE_DEVICES=3
 
 export WANDB_PROJECT="aireadi_prediction"
 export WANDB_NAME="glucose_calorie"
