0fa2e22a3fdd81f3390af1008ff7701533ecbc12
Haochen Zhang <hz112@rice.edu>
2026-03-29 20:20:34 -0500

    0329

--- ImagenTime/run_synth_u_imagen_time_text_ldm.sh
+++ ImagenTime/run_synth_u_imagen_time_text_ldm.sh
@@ -1,10 +1,20 @@
-export CUDA_VISIBLE_DEVICES=7
+export CUDA_VISIBLE_DEVICES=3
+
+#export WANDB_PROJECT="text_conditional_baselines"
+#export WANDB_NAME="LDM_debug"
+#
+#python run_text_conditional_ldm.py \
+#  --config ./configs/neurips_baseline/synth_u_text_conditional_ldm.yaml \
+#  --log_dir ../edm_results_text_conditional/LDM_debug \
+#  --model_type LDMUNetModel \
+#  --fid_vae_ckpt_path "/playpen-shared/haochenz/fid_vae_ckpts/vae_synth_u/best.pt"
+
 
 export WANDB_PROJECT="text_conditional_baselines"
 export WANDB_NAME="LDM_debug"
 
 python run_text_conditional_ldm.py \
-  --config ./configs/neurips_baseline/synth_u_text_conditional_ldm.yaml \
-  --log_dir ../edm_results_text_conditional/LDM_debug \
+  --config ./configs/neurips_baseline/synth_u_uncond_ldm.yaml \
+  --log_dir ../edm_results_unconditional/LDM \
   --model_type LDMUNetModel \
   --fid_vae_ckpt_path "/playpen-shared/haochenz/fid_vae_ckpts/vae_synth_u/best.pt"
