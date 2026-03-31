c1f1a3d00df87d871f9973060a4eecd38f7ae318
Haochen Zhang <hz112@rice.edu>
2026-03-31 14:42:33 -0500

    0331

---
+++ ImagenTime/run_ETTh1.sh
@@ -0,0 +1,9 @@
+export CUDA_VISIBLE_DEVICES=4
+#python run_unconditional.py \
+#  --config ./configs/neurips_baseline/synth_u.yaml
+
+export WANDB_PROJECT="ETTh1"
+export WANDB_NAME="VerbalTS_uncond_EDM"
+python run_unconditional.py \
+  --config ./configs/neurips_baseline/etth1.yaml \
+  --log_dir ../ETTh1_results/ImagenTime
