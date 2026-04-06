2eadc7db10dd511ba020524cf5be132b685373d7
Haochen Zhang <hz112@rice.edu>
2026-04-06 10:27:28 -0500

    0406

--- ImagenTime/run_aireadi_glucose_calorie.sh
+++ ImagenTime/run_aireadi_glucose_calorie.sh
@@ -3,9 +3,14 @@ export CUDA_VISIBLE_DEVICES=3
 export WANDB_PROJECT="aireadi_prediction"
 export WANDB_NAME="glucose_calorie"
 
+#python run_prediction.py \
+#  --config ./configs/extrapolation/aireadi_glucose_calorie.yaml \
+#  --log_dir ./results_aireadi/glucose_calorie
+
 python run_prediction.py \
   --config ./configs/extrapolation/aireadi_glucose_calorie.yaml \
-  --log_dir ./results_aireadi/glucose_calorie
+  --log_dir ./results_aireadi/glucose_calorie \
+  --resume True
 
 
 
